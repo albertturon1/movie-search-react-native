32fa9f32d29ebf9feecd3f8dc64950db1f756d51
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-26 21:11:04 +0200

    feat: added env runtime check with @t3-oss/env-core

--- src/services/moviesApi.ts
+++ src/services/moviesApi.ts
@@ -1,22 +1,22 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import Config from 'react-native-config';
 
 import {GenresResponse, MoviesResponse} from '@components/interfaces/IMovieAPi';
+import {env} from '@src/env';
 
 export const moviesApi = createApi({
   reducerPath: 'moviesApi',
-  baseQuery: fetchBaseQuery({baseUrl: Config.API_URL}),
+  baseQuery: fetchBaseQuery({baseUrl: env.API_URL}),
   endpoints: builder => ({
     searchMovies: builder.query<MoviesResponse, string>({
       query: (name: string) => ({
-        url: `/search/movie?api_key=${Config.API_KEY}&query=${name}&page=1`,
+        url: `/search/movie?api_key=${env.API_KEY}&query=${name}&page=1`,
       }),
     }),
     trendingMovies: builder.query<MoviesResponse, void>({
-      query: () => ({url: `/trending/movie/week?api_key=${Config.API_KEY}`}),
+      query: () => ({url: `/trending/movie/week?api_key=${env.API_KEY}`}),
     }),
     genres: builder.query<GenresResponse, void>({
-      query: () => ({url: `/genre/movie/list?api_key=${Config.API_KEY}`}),
+      query: () => ({url: `/genre/movie/list?api_key=${env.API_KEY}`}),
     }),
   }),
 });
