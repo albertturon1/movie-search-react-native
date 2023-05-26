ffea87ccdd3d50a7b73abae02c891084d9e3fbd5
albertturon1 <albertturon1@users.noreply.github.com>
2023-05-26 20:31:50 +0000

    apply formatting changes

--- src/components/SearchScreen/SearchBar.tsx
+++ src/components/SearchScreen/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Searchbar } from 'react-native-paper';
+import {Searchbar} from 'react-native-paper';
 
 interface Props {
   value: string;
@@ -9,6 +9,18 @@ interface Props {
   autoFocus: boolean;
 }
 
-const SearchBar: React.FC<Props> = ({ value = '', onChangeText, placeholder = 'Search for a movie', autoFocus = false }) => <Searchbar placeholder={placeholder} onChangeText={onChangeText} value={value} autoFocus={autoFocus}/>;
+const SearchBar: React.FC<Props> = ({
+  value = '',
+  onChangeText,
+  placeholder = 'Search for a movie',
+  autoFocus = false,
+}) => (
+  <Searchbar
+    placeholder={placeholder}
+    onChangeText={onChangeText}
+    value={value}
+    autoFocus={autoFocus}
+  />
+);
 
 export default SearchBar;
