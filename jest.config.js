6af4fde22aeffa232263cacd3ebf00197cc7255b
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-26 20:57:47 +0200

    fea: configured jest

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+const {pathsToModuleNameMapper} = require('ts-jest');
+
+const {compilerOptions} = require('./tsconfig');
+
+module.exports = {
+  preset: 'react-native',
+
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['<rootDir>/jestSetup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-navigation|react-native-config|react-native-reanimated|react-native-image-crop-picker|react-native-maps|react-native-linear-gradient)/.*)',
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+
+  collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx', '!**/node_modules/**', '!**/.yarn/**'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'interfaces',
+    'mocks',
+    'constants',
+    'sentry.ts',
+    'store.ts',
+  ],
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
---
+++ jestSetup.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-native/extend-expect';
+import 'react-native-gesture-handler/jestSetup';
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
