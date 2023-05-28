686218ac769d4a3b543325b72c3d797ac6526095
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-28 16:01:44 +0200

    fix: changed MoviePoster image path

--- src/screens/MoviePoster.tsx
+++ src/screens/MoviePoster.tsx
@@ -1,39 +1,24 @@
 import {View, Image} from 'react-native';
 
-import {
-  MovieImagesResponse,
-  MovieShort,
-} from '@components/interfaces/IMovieAPi';
+import {MovieShort} from '@components/interfaces/IMovieAPi';
 import {getTMDBImagePath} from '@src/lib/utils';
 
 const DEFAULT_POSTER_ASPECT_RATIO = 0.667;
 
-const MoviePoster = ({
-  images,
-  movieInitialData,
-}: {
-  images: MovieImagesResponse | undefined;
-  movieInitialData: MovieShort;
-}) => {
-  const posterUri = getTMDBImagePath({
-    path:
-      images && !!images.posters.length
-        ? images.posters[0].file_path
-        : movieInitialData.poster_path,
-    size: 'w500',
-  });
-  const posterAspectRatio =
-    images && !!images.posters.length
-      ? images.posters[0].aspect_ratio
-      : DEFAULT_POSTER_ASPECT_RATIO;
-
-  return (
-    <View
-      className="min-w-[120px] w-[30%] max-w-[300px] h-min border-0.5 border-border"
-      style={{aspectRatio: posterAspectRatio}}>
-      <Image source={{uri: posterUri}} className="object-cover w-full h-full" />
-    </View>
-  );
-};
+const MoviePoster = ({movieInitialData}: {movieInitialData: MovieShort}) => (
+  <View
+    className="min-w-[120px] w-[30%] max-w-[300px] h-min border-0.5 border-border"
+    style={{aspectRatio: DEFAULT_POSTER_ASPECT_RATIO}}>
+    <Image
+      source={{
+        uri: getTMDBImagePath({
+          path: movieInitialData.poster_path,
+          size: 'w500',
+        }),
+      }}
+      className="object-cover w-full h-full"
+    />
+  </View>
+);
 
 export default MoviePoster;
