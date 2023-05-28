8bc095ac23c3276f1eb44479ecdacbcafaf42f12
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-28 19:47:02 +0200

    fix: refactored MoviePoster

--- src/screens/MoviePoster.tsx
+++ src/screens/MoviePoster.tsx
@@ -1,18 +1,17 @@
 import {View, Image} from 'react-native';
 
-import {MovieShort} from '@components/interfaces/IMovieAPi';
 import {getTMDBImagePath} from '@src/lib/utils';
 
 const DEFAULT_POSTER_ASPECT_RATIO = 0.667;
 
-const MoviePoster = ({movieInitialData}: {movieInitialData: MovieShort}) => (
+const MoviePoster = ({path}: {path: string}) => (
   <View
     className="min-w-[120px] w-[30%] max-w-[300px] h-min border-0.5 border-border"
     style={{aspectRatio: DEFAULT_POSTER_ASPECT_RATIO}}>
     <Image
       source={{
         uri: getTMDBImagePath({
-          path: movieInitialData.poster_path,
+          path,
           size: 'w500',
         }),
       }}
