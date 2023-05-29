8a1540cbeeedbd058fb4f12cc2065ddaf14dff98
Albert <39496218+albertturon1@users.noreply.github.com>
2023-05-29 13:18:40 +0200

    fix: changed color of LoadingIndicator for light mode

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -3,7 +3,7 @@ import {ActivityIndicator, View} from 'react-native';
 export default function LoadingIndicator() {
   return (
     <View className="flex flex-1 bg-tertiaryBlack justify-center items-center">
-      <ActivityIndicator size="large" color="#fff" />
+      <ActivityIndicator size="large" />
     </View>
   );
 }
